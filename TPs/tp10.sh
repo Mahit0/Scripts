769d8b4484ca5ba91d3c735a09a0dc257685075f
matt <gitlab_admin_4e1206@example.com>
2026-04-29 17:55:27 +0000

    debut tp10

---
+++ TPs/tp10.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#################################
+# Author : Ottema05             #
+# File : creatusersMAJ.sh       #  
+# Edit : 29/04/2026             #
+# Last commit by : Ottema05     #
+#################################
+
+# On definit les variables globales
+current_path=$(pwd)
+
+# On recup les fichiers .sh 
+
